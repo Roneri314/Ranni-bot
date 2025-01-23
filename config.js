1654e879a343008969fe23c0fc86da12934f827c
Roneri314 <150666615+Roneri314@users.noreply.github.com>
2025-01-23 11:43:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ global.antitags = true;
 
 
 //auto functioner
-global.autoTyping = false;                //auto tying by default off.
-global.autoRecord = false;                //auto recording by default off.
+global.autoTyping = true;                //auto tying by default off.
+global.autoRecord = true;                //auto recording by default off.
 global.groupevent = false;                //This is the new variable for controlling group event handling.
 global.statusseen = true;                 //This is the new variable for controlling status seen.
 
